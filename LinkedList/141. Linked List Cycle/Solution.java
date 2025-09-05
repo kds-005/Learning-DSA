4877d5a9bbf95a03b0b882fc0ff0a3025443a09f
Dhayasagar K <dhayasagar18@gmail.com>
2025-09-05 16:38:59 +0530

    Added python solution for leetcode:141

--- LinkedList/141. Linked List Cycle/Solution.java
+++ LinkedList/141. Linked List Cycle/Solution.java
@@ -1,14 +1,16 @@
 /**
  * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+ *  */
+
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    } 
+}
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
