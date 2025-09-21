46304f6748465cb08bcddd454642f1ee767ba299
Dhayasagar K <dhayasagar18@gmail.com>
2025-09-21 21:11:06 +0530

    Added python solution for leetcode weeekly contest 468: Q1 (3688)

---
+++ Contest/3688. Bitwise OR of Even Numbers in an Array/Solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def evenNumberBitwiseORs(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            if num % 2 == 0:
+                res = res | num
+        return res
