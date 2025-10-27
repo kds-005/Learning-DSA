cfeccf82b9079e5b9a9c49d79a68b3ff4b71848f
Dhayasagar K <dhayasagar18@gmail.com>
2025-10-27 13:22:12 +0530

    Added java solution for leetcode: 704

---
+++ Binary Search/704. Binary Search/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // N : len(nums)
+        // Time Complexity: O(N LogN)
+        // Space Complexity: O(1)
+        int n = nums.length;
+        int l = 0;
+        int r = n - 1;
+        while (l <= r) {
+            int mid = (r + l) / 2;
+            if (target > nums[mid]) l = mid + 1;
+            else if (target < nums[mid]) r = mid - 1;
+            else return mid;
+        }
+        return -1;
+    }
+}
