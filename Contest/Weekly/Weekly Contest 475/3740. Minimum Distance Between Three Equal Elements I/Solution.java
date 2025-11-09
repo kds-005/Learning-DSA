887d97c98450d77d56eaec170f0cc44ec140ce20
Dhayasagar K <dhayasagar18@gmail.com>
2025-11-09 22:54:00 +0530

    Added java solution for weekly leetcode contest 475: Q1 (3740)

---
+++ Contest/Weekly/Weekly Contest 475/3740. Minimum Distance Between Three Equal Elements I/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.Math;
+
+class Solution {
+    public int minimumDistance(int[] nums) {
+        // N : len(nums)
+        // Time Complexity: O(N)
+        // Space Complexity: O(N)
+        int n = nums.length;
+        HashMap<Integer, List<Integer>> freq = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> temp = freq.getOrDefault(nums[i], new ArrayList<>());
+            temp.add(i);
+            freq.put(nums[i], temp);
+        }
+
+        int minDist = Integer.MAX_VALUE;
+        for (int key: freq.keySet()) {
+            List<Integer> val = freq.get(key);
+            int m = val.size();
+            int currDist = 0;
+            if (m >= 3) {
+                for (int i = 2; i < m; i++) {
+                    currDist = Math.abs(val.get(i - 2) - val.get(i - 1)) + 
+                                Math.abs(val.get(i - 1) - val.get(i)) + 
+                                Math.abs(val.get(i) - val.get(i - 2));
+                    minDist = Math.min(minDist, currDist);
+                }
+            }
+        }
+        return (minDist != Integer.MAX_VALUE) ? minDist : -1;
+    }
+}
