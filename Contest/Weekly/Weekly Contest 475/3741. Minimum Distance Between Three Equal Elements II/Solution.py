0ed0bba2de9ca5e0476cb23de2ab8523be806bcf
Dhayasagar K <dhayasagar18@gmail.com>
2025-11-09 22:57:35 +0530

    Added python solution for weekly leetcode contest 475: Q2 (3741)

---
+++ Contest/Weekly/Weekly Contest 475/3741. Minimum Distance Between Three Equal Elements II/Solution.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        # N : len(nums)
+        # Time Complexity: O(N)
+        # Space Complexity: O(N)
+        n = len(nums)
+        freq = defaultdict(list)
+        for i in range(n):
+            freq[nums[i]].append(i)
+
+        minDist = float('inf')
+        for key, val in freq.items():
+            if len(val) < 3:
+                continue
+            for i in range(2, len(val)):
+                dist = abs(val[i - 2] - val[i - 1]) + abs(val[i - 1] - val[i]) + abs(val[i] - val[i - 2])
+                minDist = min(minDist, dist)
+        return minDist if minDist != float('inf') else -1
