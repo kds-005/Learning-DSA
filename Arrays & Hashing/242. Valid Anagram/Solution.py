95090e781f738af8bb26f7523a1e73d2cbcd63c5
Dhayasagar K <dhayasagar18@gmail.com>
2025-11-16 21:27:21 +0530

    Added python solution for leetcode: 242

---
+++ Arrays & Hashing/242. Valid Anagram/Solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # M : len(s), N : len(t)
+        # Time Complexity: O(max(logM, logN))
+        # Space Complexity: O(1)
+        # s = sorted(s)
+        # t = sorted(t)
+        # return s == t
+
+
+        # M : len(s), N : len(t)
+        # Time Complexity: O(max(M, N))
+        # Space Complexity: O(26)
+        sFreq = [0] * 26
+        
+        for ch in s:
+            sFreq[ord(ch) - ord('a')] += 1
+
+        for ch in t:
+            sFreq[ord(ch) - ord('a')] -= 1
+        
+        for i in range(26):
+            if sFreq[i] != 0: 
+                return False
+        return True
